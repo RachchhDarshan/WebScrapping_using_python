e9fb1ac1369a839b43a8586c9ff9aef5294ba1e7
RachchhDarshan <darshanrachchh2003@gmail.com>
2024-06-28 14:21:39 +0530

    final commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd # type: ignore
+import requests # type: ignore
+from bs4 import BeautifulSoup # type: ignore
+import json
+from io import StringIO
+
+
+
+for i in range(1,3):
+    url = "https://www.flipkart.com/search?q=laptop&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page="+str(i)
+
+    r = requests.get(url)
+                    # print(r)
+    soup = BeautifulSoup(r.text, "lxml")
+        # box = soup.find("div",class_ = "DOjaWF gdgoEp")
+    
+    products_tag = soup.findAll("div" , class_="tUxRFH")
+    
+    productdic = {}
+    
+    for product in  products_tag:
+        
+
+        names = product.find("div",class_ = "KzDlHZ")
+
+
+        prices = product.find("div",class_ = "Nx9bqj _4b5DiR")
+
+        productdic["name"] = names.text.strip()
+        productdic["price"] = prices.text.strip()
+        
+        print(productdic)
+    
+
+
+
+    
+    
+    
